4df1008cbc2f9ef40e5d2e9024c30bddc8a05537
Igor Parfenov <lloopy123123543@gmail.com>
2023-07-13 15:01:17 +0500

    dropdown

--- front/components/Nav.jsx
+++ front/components/Nav.jsx
@@ -8,6 +8,14 @@ import Link from 'next/link';
 function Nav() {
   const isUserLoggedIn = true
   const [providers, setProviders] = useState(null)
+  const [toggleDropdown, setToggleDropdown] = useState(false)
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+      setProviders(response)
+    }
+  },[])
   return (
 
     <nav className='flex-between w-full mb-16 pt-3'>
@@ -66,10 +74,39 @@ function Nav() {
                     <Image src="/assets/images/avatar.jpg"
                     className="rounded-full"
         alt="Prompt"
-        width={30}
-        height={30}
+        width={37}
+        height={37}
+        onClick={() => setToggleDropdown((prev) => !prev)}
         >
         </Image>
+
+        {toggleDropdown && (
+          <div className="dropdown">
+            <Link href={"/profile"}
+            className="dropdown_link"
+            onClick={() => setToggleDropdown(false)}
+            >
+              My Profile
+            </Link>
+            <Link href={"/profile"}
+            className="dropdown_link"
+            onClick={() => setToggleDropdown(false)}
+            >
+              Create prompt
+            </Link>
+            <button
+            type='button'
+            onClick={() => {
+              setToggleDropdown(false)
+              signOut();
+            }}
+            className='mt-5 w-full black_btn'
+            >
+              Sing out
+            </button>
+
+          </div>
+        )}
           </div>
         ): (<>
           {providers && Object.values(providers).map((provider) => (
